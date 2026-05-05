d58278aea446fb04835fea53256f1cba0b9b4d1b
taiyakinosakebi <taichi20051219@gmail.com>
2026-05-05 23:14:24 +0900

    ログ機能の削除

--- index.js
+++ index.js
@@ -68,13 +68,4 @@ const server = http
         break;
     }
   })
-  .on('error', e => {
-    console.error(`[${new Date()}] Server Error`, e);
-  })
-  .on('clientError', e => {
-    console.error(`[${new Date()}] Client Error`, e);
-  });
 const port = process.env.PORT || 8000;
-server.listen(port, () => {
-  console.info(`[${new Date()}] Listening on ${port}`);
-});
