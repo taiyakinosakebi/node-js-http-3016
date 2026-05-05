205b9238c8fff5e6d0fa513bb9a11183674db49c
taiyakinosakebi <taichi20051219@gmail.com>
2026-05-05 23:21:07 +0900

    ログ機能の削除

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('node:http');
 const pug = require('pug');
 const server = http
   .createServer((req, res) => {
+        console.info(`Requested by ${req.socket.remoteAddress}`);
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8'
     });
@@ -57,7 +58,7 @@ const server = http
           .on('end', () => {
             const answer = new URLSearchParams(rawData);
             const body = `${answer.get('name')}さんは${answer.get('favorite')}に投票しました`;
-            console.info(`[${now}] ${body}`);
+            console.info(body);
             res.write(
               `<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`
             );
@@ -68,4 +69,13 @@ const server = http
         break;
     }
   })
+  .on('error', e => {
+    console.error('Server Error', e);
+  })
+  .on('clientError', e => {
+    console.error('Client Error', e);
+  });
 const port = process.env.PORT || 8000;
+server.listen(port, () => {
+  console.info(`Listening on ${port}`);
+});
